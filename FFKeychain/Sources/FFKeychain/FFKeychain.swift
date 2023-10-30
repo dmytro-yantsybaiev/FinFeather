b0d574fdba36c6bb82cf841adc221f933066e9d1
dy-owl <d.yantsybaiev@gmail.com>
2023-10-30 21:12:00 +0200

    minor updates

--- FFKeychain/Sources/FFKeychain/FFKeychain.swift
+++ FFKeychain/Sources/FFKeychain/FFKeychain.swift
@@ -26,7 +26,7 @@ public final class FFKeychain {
     @discardableResult
     public func save<Item: Codable>(_ item: Item, key: Key) -> Item? {
         guard let data: Data = try? JSONEncoder.shared.encode(item) else {
-            logger.fault("Failed to encode the item `\(Item.self)` for the key `\(key.rawValue)`.")
+            logger.fault("🔑🔒: Failed to encode the item `\(Item.self)` for the key `\(key.rawValue)`.")
             return nil
         }
 
@@ -40,7 +40,7 @@ public final class FFKeychain {
         let status: OSStatus = SecItemAdd(attributes, nil)
 
         if status == errSecSuccess {
-            logger.info("The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully saved.")
+            logger.info("🔑🔒: The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully saved.")
             return item
         }
 
@@ -49,11 +49,10 @@ public final class FFKeychain {
         }
 
         let errorMessage: String = (SecCopyErrorMessageString(status, nil) as? String).orEmpty
-        logger.fault("Failed to save the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
+        logger.fault("🔑🔒: Failed to save the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
         return nil
     }
 
-    @discardableResult
     public func loadItem<Item: Codable>(key: Key) -> Item? {
         let query: CFDictionary = [
             kSecClass: kSecClassGenericPassword,
@@ -67,21 +66,21 @@ public final class FFKeychain {
 
         guard status == errSecSuccess else {
             let errorMessage: String = (SecCopyErrorMessageString(status, nil) as? String).orEmpty
-            logger.fault("Failed to load the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
+            logger.fault("🔑🔒: Failed to load the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
             return nil
         }
 
         guard let data: Data = result as? Data else {
-            logger.fault("Failed to parse the found result as `Data` for the key `\(key.rawValue)`.")
+            logger.fault("🔑🔒: Failed to parse the found result as `Data` for the key `\(key.rawValue)`.")
             return nil
         }
 
         guard let item: Item = try? JSONDecoder.shared.decode(Item.self, from: data) else {
-            logger.fault("Failed to decode the item's data as `\(Item.self)` for the key `\(key.rawValue)`.")
+            logger.fault("🔑🔒: Failed to decode the item's data as `\(Item.self)` for the key `\(key.rawValue)`.")
             return nil
         }
 
-        logger.info("The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully loaded.")
+        logger.info("🔑🔒: The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully loaded.")
         return item
     }
 
@@ -96,11 +95,11 @@ public final class FFKeychain {
 
         guard status == errSecSuccess else {
             let errorMessage: String = (SecCopyErrorMessageString(status, nil) as? String).orEmpty
-            logger.fault("Failed to delete the keychain item for the key `\(key.rawValue)`. \(errorMessage)")
+            logger.fault("🔑🔒: Failed to delete the keychain item for the key `\(key.rawValue)`. \(errorMessage)")
             return
         }
 
-        logger.info("The keychain item for the key `\(key.rawValue)` was successfully deleted.")
+        logger.info("🔑🔒: The keychain item for the key `\(key.rawValue)` was successfully deleted.")
     }
 
     public func clear() {
@@ -109,7 +108,7 @@ public final class FFKeychain {
 
     private func update<Item: Codable>(_ item: Item, key: Key) -> Item? {
         guard let data: Data = try? JSONEncoder.shared.encode(item) else {
-            logger.fault("Failed to encode the item `\(Item.self)` for the key `\(key.rawValue)`.")
+            logger.fault("🔑🔒: Failed to encode the item `\(Item.self)` for the key `\(key.rawValue)`.")
             return nil
         }
 
@@ -124,11 +123,11 @@ public final class FFKeychain {
 
         guard status == errSecSuccess else {
             let errorMessage: String = (SecCopyErrorMessageString(status, nil) as? String).orEmpty
-            logger.fault("Failed to update the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
+            logger.fault("🔑🔒: Failed to update the keychain item `\(Item.self)` for the key `\(key.rawValue)`. \(errorMessage)")
             return nil
         }
 
-        logger.info("The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully updated.")
+        logger.info("🔑🔒: The keychain item `\(Item.self)` for the key `\(key.rawValue)` was successfully updated.")
         return item
     }
 }
@@ -138,7 +137,7 @@ private extension FFKeychain.Key {
     var account: String {
         switch self {
         case .test:
-            .empty
+            "Test"
         case .user:
             Bundle(for: User.self).bundleIdentifier.orEmpty
         }
