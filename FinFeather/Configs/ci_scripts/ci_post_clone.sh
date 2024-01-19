a0a8a17af93bbdfd6d88b4a91ab7ba23c8d000de
dmytro-yantsybaiev <d.yantsybaiev@gmail.com>
2024-01-20 01:57:21 +0200

    Implement the ci_post_clone.sh script

---
+++ FinFeather/Configs/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  FinFeather
+#
+#  Created by Dmytro Yantsybaiev on 20.01.2024.
+#  
+
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
